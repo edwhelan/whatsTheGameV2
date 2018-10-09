1dd9835ab0b1265567f17f691e4c95185105145a
Edward Whelan <vroxing@gmail.com>
2018-10-09 09:08:01 -0400

    add styles to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,9 +5,18 @@ import Layout from '../components/layout'
 
 const AboutPage = () => (
   <Layout>
-    <h3>Hi from the second page</h3>
-    <p>I built this site as a tool for me to easily check the current scores of the day and see what time games start. If you are interested in my work please check out <a href='http://www.EdWhe.com'> EdWhe.com </a>.</p>
-    <Link to="/">Go back to the games</Link>
+    <div
+    className= 'about-page'
+    >
+      <h3>About me and this project</h3>
+      <p>I built this site as a tool for me to easily check the current scores of the day and see what time games start. If you are interested in my work please check out <a href='http://www.EdWhe.com'> EdWhe.com </a>.</p>
+      <Link to="/"
+      style={{
+        textDecoration: 'none',
+        color: 'white',
+      }}
+      >Go back to the games</Link>
+    </div>
   </Layout>
 )
 
